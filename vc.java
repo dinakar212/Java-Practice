05e077d5ec7e9115388ab1b2b8a3a45a2337bf38
dinakar212 <dinakar.19kalidindi@gmail.com>
2016-07-29 01:26:46 +0530

    Create vc.java

---
+++ vc.java
@@ -0,0 +1,11 @@
+class VC
+{
+public static void main(String []x)
+{
+ char c;
+ if(c=='A'||c=='O'||c=='E'||c=='I'||c=='U'||c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+ System.out.println("it is a vowel");
+ else
+ System.out.println("it is a consonant");
+ }
+ }
