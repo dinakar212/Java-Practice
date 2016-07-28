7d0c531d5245e1f066103f1f494df32caee5630d
dinakar212 <dinakar.19kalidindi@gmail.com>
2016-07-29 02:16:52 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.util.Scanner.*;
+class Natural
+{
+public static void main(String []x)
+{
+ int n,i,s=0;
+System.out.println("ENTER THE NUMBER");
+Scanner in = new Scanner(System.in);
+n=in.nextInt();
+for(i=0;i<=n;i++)
+{
+ s=s+i;
+ }
+ System.out.println("The sum of natural numbers is " +c);
+ }
+ }
