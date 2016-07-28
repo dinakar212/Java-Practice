04f6023647cabade3fe509b3cac386aefcd53f6c
dinakar212 <dinakar.19kalidindi@gmail.com>
2016-07-29 01:51:05 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Large
+{
+public static void main(String []x)
+{
+ int x,y,z;
+System.out.println("ENTER THE NUMBER");
+Scanner in = new Scanner(System.in);
+x=in.nextInt();
+y=in.nextInt();
+z=in.nextInt();
+if(a>b)
+{
+  if(a>c)
+System.out.println("the largest of the three is " +a);
+ else
+System.out.println("the largest of the three is " +c);
+}
+else
+{
+ if(b>c)
+System.out.println("the largest of the three is " +b);
+ else
+System.out.println("the largest of the three is " +c);
+} 
+}
+}
+  
