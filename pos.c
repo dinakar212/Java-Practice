e26b9905fb519c193b6e695ec72827815b5fd69a
dinakar212 <dinakar.19kalidindi@gmail.com>
2016-07-28 19:27:06 +0000

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+{
+ int x;
+scanf("%d",&x);
+if(x>0)
+{
+ printf("positive number");
+}
+elseif(x<0)
+{
+printf("negative number");
+}
+else
+printf("non categorized whole number");
