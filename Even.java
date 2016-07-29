f8a8ab4830567bcd139780b69dc19b98067007cb
dinakar212 <dinakar.19kalidindi@gmail.com>
2016-07-29 23:42:26 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,19 @@
+import java.util.Scanner.*;
+class Even
+{
+public static void main(String []x)
+{
+ int a,b;
+System.out.println("ENTER THE NUMBER");
+Scanner in = new Scanner(System.in);
+a=in.nextInt();
+b=in.nextInt();
+while(a<b)
+{
+ i=a+1;
+ if(i%2==0)
+ System.out.println(i);
+ a++;
+ }
+ }
+}
